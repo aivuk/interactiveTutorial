e3754e9b5351feeec4a2cd6d0442545ada9b9eca
Edgar Zanella Alvarenga <e@vaz.io>
2022-12-16 22:04:54 +0100

    update sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
   import '../app.css'
 
-  //export const prerender = true;
-  //export const ssr = true;
+  export const prerender = true;
+  export const ssr = false;
   export const trailingSlash = 'ignore';
 </script>
 
